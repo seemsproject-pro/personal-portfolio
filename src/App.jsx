65ab354dd83172e8c98844418d7fdbabdd54176d
seemsproject-pro <syamsdzq@gmail.com>
2026-07-09 10:17:29 +0700

    Fix images and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Portfolio from './pages/Portfolio';
 import Admin from './pages/Admin';
 import { DataProvider } from './context/DataContext';
@@ -7,10 +7,11 @@ import { DataProvider } from './context/DataContext';
 function App() {
   return (
     <DataProvider>
-      <Router basename="/personal-portfolio">
+      <Router>
         <Routes>
           <Route path="/" element={<Portfolio />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<Portfolio />} />
         </Routes>
       </Router>
     </DataProvider>
